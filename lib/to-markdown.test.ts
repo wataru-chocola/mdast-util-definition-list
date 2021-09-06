774fe0c7fe00fd35faf1d78b8232870326a2ad1d
Wataru Watanabe <wataru.chocola@gmail.com>
2021-09-06 13:56:55 +0900

    add skelton code for to-markdown utility

---
+++ lib/to-markdown.test.ts
@@ -0,0 +1,146 @@
+import { defListToMarkdown } from './to-markdown';
+import { toMarkdown } from 'mdast-util-to-markdown';
+import { defList } from 'micromark-extension-definition-list';
+
+const compile = (mdast: any) =>
+  toMarkdown(mdast, {
+    extensions: [defListToMarkdown],
+  });
+
+test('mdast -> markdown', () => {
+  const mdast = {
+    type: 'root',
+    children: [
+      {
+        type: 'paragraph',
+        children: [
+          {
+            type: 'text',
+            value: 'Test for defList.',
+            position: {
+              start: { column: 1, line: 2, offset: 1 },
+              end: { column: 18, line: 2, offset: 18 },
+            },
+          },
+        ],
+        position: {
+          start: { column: 1, line: 2, offset: 1 },
+          end: { column: 18, line: 2, offset: 18 },
+        },
+      },
+      {
+        type: 'defList',
+        children: [
+          {
+            type: 'defListTerm',
+            children: [
+              {
+                type: 'text',
+                value: 'Apple',
+                position: {
+                  start: { column: 1, line: 4, offset: 20 },
+                  end: { column: 6, line: 4, offset: 25 },
+                },
+              },
+            ],
+            position: {
+              start: { column: 1, line: 4, offset: 20 },
+              end: { column: 1, line: 5, offset: 26 },
+            },
+          },
+          {
+            type: 'defListDescription',
+            spread: false,
+            children: [
+              {
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    value: 'Pomaceous fruit of plants of the genus Malus in\nthe family Rosaceae.',
+                    position: {
+                      start: { column: 5, line: 5, offset: 30 },
+                      end: { column: 25, line: 6, offset: 102 },
+                    },
+                  },
+                ],
+                position: {
+                  start: { column: 5, line: 5, offset: 30 },
+                  end: { column: 25, line: 6, offset: 102 },
+                },
+              },
+            ],
+            position: {
+              start: { column: 1, line: 5, offset: 26 },
+              end: { column: 1, line: 8, offset: 104 },
+            },
+          },
+          {
+            type: 'defListTerm',
+            children: [
+              {
+                position: {
+                  start: { column: 1, line: 8, offset: 104 },
+                  end: { column: 7, line: 8, offset: 110 },
+                },
+                type: 'text',
+                value: 'Orange',
+              },
+            ],
+            position: {
+              start: { column: 1, line: 8, offset: 104 },
+              end: { column: 1, line: 9, offset: 111 },
+            },
+          },
+          {
+            type: 'defListDescription',
+            spread: false,
+            children: [
+              {
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    value: 'The fruit of an evergreen tree of the genus Citrus.',
+                    position: {
+                      start: { column: 5, line: 9, offset: 115 },
+                      end: { column: 56, line: 9, offset: 166 },
+                    },
+                  },
+                ],
+                position: {
+                  start: { column: 5, line: 9, offset: 115 },
+                  end: { column: 56, line: 9, offset: 166 },
+                },
+              },
+            ],
+            position: {
+              start: { column: 1, line: 9, offset: 111 },
+              end: { column: 1, line: 10, offset: 167 },
+            },
+          },
+        ],
+        position: {
+          start: { column: 1, line: 4, offset: 20 },
+          end: { column: 1, line: 10, offset: 167 },
+        },
+      },
+    ],
+    position: {
+      start: { column: 1, line: 1, offset: 0 },
+      end: { column: 1, line: 10, offset: 167 },
+    },
+  };
+  const md = `
+Test for defList.
+
+Apple
+:   Pomaceous fruit of plants of the genus Malus in
+    the family Rosaceae.
+
+Orange
+:   The fruit of an evergreen tree of the genus Citrus.
+`;
+  const result = compile(mdast);
+  expect(result).toEqual(md);
+});
--- lib/to-markdown.ts
+++ lib/to-markdown.ts
@@ -1,1 +1,21 @@
-export const defListToMarkdown = {};
+import type { Handle } from 'mdast-util-to-markdown';
+
+const defListHandler: Handle = (node, _parent, context) => {
+  return '';
+};
+
+const defListTermHandler: Handle = (node, _parent, context) => {
+  return '';
+};
+
+const defListDescriptionHandler: Handle = (node, _parent, context) => {
+  return '';
+};
+
+export const defListToMarkdown = {
+  handlers: {
+    defList: defListHandler,
+    defListTerm: defListTermHandler,
+    defListDescription: defListDescriptionHandler,
+  },
+};
