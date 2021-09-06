879050efef020fa8b8811f03e8e4d103bbf0500d
Wataru Watanabe <wataru.chocola@gmail.com>
2021-09-06 12:47:37 +0900

    export to-hast utility functions

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 export { defListFromMarkdown } from './lib/from-markdown';
 export { defListToMarkdown } from './lib/to-markdown';
+export {
+  defListHastHandlers,
+  mdastDefList2hast,
+  mdastDefListTerm2hast,
+  mdastDefListDescription2hast,
+} from './lib/to-hast';
