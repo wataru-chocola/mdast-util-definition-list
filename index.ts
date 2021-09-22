5dc52c98e2fa5e450ad7327ab2dbe5d9fc878d75
Wataru Watanabe <wataru.chocola@gmail.com>
2021-09-22 13:51:05 +0900

    add file extension in import for nodejs and browser ESM compatibility

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-export { defListFromMarkdown } from './lib/from-markdown';
-export { defListToMarkdown } from './lib/to-markdown';
+export { defListFromMarkdown } from './lib/from-markdown.js';
+export { defListToMarkdown } from './lib/to-markdown.js';
 export {
   defListHastHandlers,
   mdastDefList2hast,
   mdastDefListTerm2hast,
   mdastDefListDescription2hast,
-} from './lib/to-hast';
+} from './lib/to-hast.js';
