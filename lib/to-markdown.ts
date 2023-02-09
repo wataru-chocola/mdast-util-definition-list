d65b1eb00ac55573c64e94a8d68e64cb5f5080a5
Wataru Watanabe <wataru.chocola@gmail.com>
2023-02-09 17:15:00 +0900

    chore(deps): update dependency mdast-util-to-markdown to v1.5.0

--- lib/to-markdown.ts
+++ lib/to-markdown.ts
@@ -1,27 +1,32 @@
 import type { Handle, Join, Options } from 'mdast-util-to-markdown';
-import { containerPhrasing } from 'mdast-util-to-markdown/lib/util/container-phrasing.js';
-import { containerFlow } from 'mdast-util-to-markdown/lib/util/container-flow.js';
-import { indentLines } from 'mdast-util-to-markdown/lib/util/indent-lines.js';
 
-const defListHandler: Handle = (node, _parent, context) => {
-  const exit = context.enter('defList');
-  const value = containerFlow(node, context);
+declare module 'mdast-util-to-markdown' {
+  interface ConstructNameMap {
+    defList: 'defList';
+    defListTerm: 'defListTerm';
+    defListDescription: 'defListDescription';
+  }
+}
+
+const defListHandler: Handle = (node, _parent, state, info) => {
+  const exit = state.enter('defList');
+  const value = state.containerFlow(node, info);
   exit();
   return value;
 };
 
-const defListTermHandler: Handle = (node, _parent, context) => {
-  const exit = context.enter('defListTerm');
-  const subexit = context.enter('phrasing');
-  const value = containerPhrasing(node, context, { before: '\n', after: '\n' });
+const defListTermHandler: Handle = (node, _parent, state, info) => {
+  const exit = state.enter('defListTerm');
+  const subexit = state.enter('phrasing');
+  const value = state.containerPhrasing(node, { ...info, before: '\n', after: '\n' });
   subexit();
   exit();
   return value;
 };
 
-const defListDescriptionHandler: Handle = (node, _parent, context) => {
-  const exit = context.enter('defListDescription');
-  const value = indentLines(containerFlow(node, context), map);
+const defListDescriptionHandler: Handle = (node, _parent, state, info) => {
+  const exit = state.enter('defListDescription');
+  const value = state.indentLines(state.containerFlow(node, info), map);
   exit();
   return value;
 
@@ -34,7 +39,7 @@ const defListDescriptionHandler: Handle = (node, _parent, context) => {
   }
 };
 
-const joinDefItems: Join = (left, right, parent, _context) => {
+const joinDefItems: Join = (left, right, parent, _state) => {
   if (parent.type !== 'defList') {
     return;
   }
