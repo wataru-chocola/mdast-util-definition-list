28c45092d7fe7fee00a5b79daf166190d0da775b
Wataru Watanabe <wataru.chocola@gmail.com>
2023-03-09 10:45:13 +0900

    chore(types): improve typescript types

--- lib/types.ts
+++ lib/types.ts
@@ -1,18 +1,24 @@
 import type { Node } from 'unist';
-import type { Parent } from 'mdast';
+import type { Parent, PhrasingContent, BlockContent, DefinitionContent } from 'mdast';
+
+type FlowContent = BlockContent | DefinitionContent;
+
+export const DefList = 'defList';
+export const DefListTerm = 'defListTerm';
+export const DefListDescription = 'defListDescription';
 
 export interface DefListNode extends Node {
-  type: 'defList';
+  type: typeof DefList;
   children: (DefListTermNode | DefListDescriptionNode)[];
 }
 export interface DefListTermNode extends Parent {
-  type: 'defListTerm';
-  children: [];
+  type: typeof DefListTerm;
+  children: PhrasingContent[];
 }
 export interface DefListDescriptionNode extends Parent {
-  type: 'defListDescription';
+  type: typeof DefListDescription;
   spread: boolean;
-  children: [];
+  children: FlowContent[];
 }
 
 declare module 'mdast' {
